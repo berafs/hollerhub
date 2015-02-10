2bb933244a8ce18db4a29a088c8392bea134daac
Kasey Uhlenhuth <kaseyu@microsoft.com>
2015-02-10 13:34:42 -0800

    github oauth added

---
+++ HollerHub2/HollerHub/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Octokit;
+using System.Threading.Tasks;
+using HollerHub.Models;
+using System.Web.Security;
+
+namespace HollerHub.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //github api info
+        //code adapted from haacked's octokit demo
+        const string clientId = "1c230d149965dc938e05";
+        private const string clientSecret = "506ff51bd78e776444a33b2b3d63049fe4118063";
+        readonly GitHubClient client = new GitHubClient(new ProductHeaderValue("OAuth"));
+
+
+        public async Task<ActionResult> Index()
+        {
+            var accessToken = Session["OAuthToken"] as string;
+            if (accessToken != null)
+            {
+                // This allows the client to make requests to the GitHub API on the user's behalf
+                // without ever having the user's OAuth credentials.
+                client.Credentials = new Credentials(accessToken);
+            }
+
+            try
+            {
+                // The following requests retrieves all of the user's repositories and
+                // requires that the user be logged in to work.
+                var repositories = await client.Repository.GetAllForCurrent();
+                var model = new IndexViewModel(repositories);
+
+                return View(model);
+            }
+            catch (AuthorizationException)
+            {
+                // Either the accessToken is null or it's invalid. This redirects
+                // to the GitHub OAuth login page. That page will redirect back to the
+                // Authorize action.
+                return Redirect(GetOauthLoginUrl());
+            }
+        }
+
+        public async Task<ActionResult> Authorize(string code, string state)
+        {
+            if (!String.IsNullOrEmpty(code))
+            {
+                var expectedState = Session["CSRF:State"] as string;
+                if (state != expectedState) throw new InvalidOperationException("SECURITY FAIL!");
+                Session["CSRF:State"] = null;
+
+                var token = await client.Oauth.CreateAccessToken(
+                    new OauthTokenRequest(clientId, clientSecret, code));
+                Session["OAuthToken"] = token.AccessToken;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private string GetOauthLoginUrl()
+        {
+            string csrf = Membership.GeneratePassword(24, 1);
+            Session["CSRF:State"] = csrf;
+
+            // 1. Redirect users to request GitHub access
+            var request = new OauthLoginRequest(clientId)
+            {
+                Scopes = { "user", "notifications" },
+                State = csrf
+            };
+            var oauthLoginUrl = client.Oauth.GetGitHubLoginUrl(request);
+            return oauthLoginUrl.ToString();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HollerHub2/HollerHub/Models/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿﻿using System.Collections.Generic;
+using Octokit;
+
+namespace HollerHub.Models
+{
+    public class IndexViewModel
+    {
+        public IndexViewModel(IEnumerable<Repository> repositories)
+        {
+            Repositories = repositories;
+        }
+
+        public IEnumerable<Repository> Repositories { get; private set; }
+    }
+}
