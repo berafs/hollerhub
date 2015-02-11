5ac20fc408a1b70bda9453d1517c8626ecd41e29
unknown <dyhi@t-gadewe02.redmond.corp.microsoft.com>
2015-02-10 19:31:21 -0800

    blaaaaahhhhhh

--- HollerHubProject/HollerHubProject/App_Start/WebApiConfig.cs
+++ HollerHubProject/HollerHubProject/App_Start/WebApiConfig.cs
@@ -5,11 +5,26 @@ using System.Web.Http;
 
 namespace HollerHubProject
 {
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var jsonSettings = new JsonSerializerSettings
+            {
+                Converters = new JsonConverter[] { new StringEnumConverter() },
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            config.Formatters.JsonFormatter.SerializerSettings = jsonSettings;
+            JsonConvert.DefaultSettings = () => jsonSettings;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- HollerHubProject/HollerHubProject/Controllers/GitHubController.cs
+++ HollerHubProject/HollerHubProject/Controllers/GitHubController.cs
@@ -4,30 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Octokit;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace HollerHubProject.Controllers
 {
     public class GitHubController : ApiController
-    {
-        const string clientId = "1c230d149965dc938e05";
-        const string clientSecret = "506ff51bd78e776444a33b2b3d63049fe4118063";
-        readonly GitHubClient client = new GitHubClient(new ProductHeaderValue("HollerHub"));
-        
-
+    {        
+        private Uri gitHubBaseUri = new Uri("https://api.github.com");
+        const string repoFmt = "/repos/{0}/{1}";
+        const string auth = "?client_id=1c230d149965dc938e05&client_secret=506ff51bd78e776444a33b2b3d63049fe4118063";
 
 
         //Given name of repo, return the repo
         //Arguments: owner/name
         [HttpGet]
         [Route("~/repo/{owner}/{name}")]
-        public async Task<IHttpActionResult> GetRepoInfo(string owner, string name)
+        public Task<HttpResponseMessage> GetRepoInfo(string owner, string name)
         {
-            var url = string.Format("/repo/{0}/{1}?=client_id={2}&client_secret={3}", owner, name, clientId, clientSecret);
-            var client = new HttpClient() { BaseAddress = new Uri("https://api.github.com") };
-            var resp = await client.GetAsync(url);
-            return Ok(resp.Content);
+            var client = new HttpClient() { BaseAddress = gitHubBaseUri };
+            //client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "Fiddler");
+            client.DefaultRequestHeaders.Remove("Connection");
+            
+            var resp = client.GetAsync(makeRepoRequestUri(owner,name));
+            return resp;
+        }
+
+        private string makeRepoRequestUri(string owner, string name)
+        {
+            return string.Format(repoFmt, owner, name) + auth;
         }
 
     }
