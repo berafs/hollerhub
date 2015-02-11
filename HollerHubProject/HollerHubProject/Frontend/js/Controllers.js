5a505dc09d2c58977741a7cad0e49367dc78c6ef
Ben Rafshoon <berafs@microsoft.com>
2015-02-11 11:04:47 -0800

    Connect to backend

--- HollerHubProject/HollerHubProject/Frontend/js/Controllers.js
+++ HollerHubProject/HollerHubProject/Frontend/js/Controllers.js
@@ -8,16 +8,29 @@ app.controller("HomeController", function ($scope) {
     };
 });
 
-app.controller("RepoController", ["$scope", "$routeParams", function ($scope, $routeParams) {
+app.controller("RepoController", ["$scope", "$routeParams", "$http", function ($scope, $routeParams, $http) {
+
     $scope.repo = {
-        name: $routeParams["repoId"]
+       
     };
+    var owner = $routeParams["owner"];
+    var repoName = $routeParams["repoName"];
+
+    $http.get("/repo/" + owner + "/" + repoName).success(function (repoData) {
+        console.log(repoData);
+        $scope.repo.name = repoData.Name;
+
+        $http.get("/api/reviews/" + repoData.Id).success(function (reviewsData) {
+            console.log(reviewsData);
+        });
+    });
+
 }]);
 
 
 app.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.
-        when("/repo/:repoId", {
+        when("/repo/:owner/:repoName", {
             "templateUrl": "Repo.html",
             "controller": "RepoController"
         }).
