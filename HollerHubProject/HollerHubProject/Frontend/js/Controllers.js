e93a8f8ecbcf17f7e4b4fca3e8bce7544684f429
Ben Rafshoon <berafs@microsoft.com>
2015-02-11 10:49:32 -0800

    New project

---
+++ HollerHubProject/HollerHubProject/Frontend/js/Controllers.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("hollerhub", ["ngRoute"]);
+
+
+
+app.controller("HomeController", function ($scope) {
+    $scope.repo = {
+        name: "Repo name"
+    };
+});
+
+app.controller("RepoController", ["$scope", "$routeParams", function ($scope, $routeParams) {
+    $scope.repo = {
+        name: $routeParams["repoId"]
+    };
+}]);
+
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+        when("/repo/:repoId", {
+            "templateUrl": "Repo.html",
+            "controller": "RepoController"
+        }).
+        when("/home", {
+            "templateUrl": "Home.html",
+            "controller": "HomeController"
+        }).
+        otherwise({
+            "redirectTo": "/home"
+        });
+}]);
