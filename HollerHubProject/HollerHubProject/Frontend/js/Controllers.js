bcb6295255c2095d86721cc54e1cbc4a18ed7445
Ben Rafshoon <berafs@microsoft.com>
2015-02-11 11:42:10 -0800

    Frontend ratings

--- HollerHubProject/HollerHubProject/Frontend/js/Controllers.js
+++ HollerHubProject/HollerHubProject/Frontend/js/Controllers.js
@@ -6,12 +6,13 @@ app.controller("HomeController", function ($scope) {
     $scope.repo = {
         name: "Repo name"
     };
+
 });
 
 app.controller("RepoController", ["$scope", "$routeParams", "$http", function ($scope, $routeParams, $http) {
 
     $scope.repo = {
-       
+        stars: []
     };
     var owner = $routeParams["owner"];
     var repoName = $routeParams["repoName"];
@@ -20,8 +21,32 @@ app.controller("RepoController", ["$scope", "$routeParams", "$http", function ($
         console.log(repoData);
         $scope.repo.name = repoData.Name;
 
+        var rating = 5;
+
+        var updateStars = function(rating) {
+            var stars = [];
+            for (var i = 0; i < 10; i++) {
+                stars[i] = {};
+                var currentStar = i + 1;
+                stars[i].lit = currentStar <= rating;
+            }
+            $scope.repo.stars = stars;
+        }
+
+        updateStars(5);
+
+        $scope.changeRating = function (index) {
+            updateStars(index);
+        };
+
         $http.get("/api/reviews/" + repoData.Id).success(function (reviewsData) {
             console.log(reviewsData);
+            var sum = reviewsData.reduce(function (previousValue, aReview) {
+                return previousValue + aReview.ratingStars;
+            }, 0.0);
+            var average = sum / reviewsData.length;
+            $scope.repo.communityRating = average.toFixed(1);
+            console.log(average);
         });
     });
 
